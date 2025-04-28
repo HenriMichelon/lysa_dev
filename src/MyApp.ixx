0a8ffd48fbc821cc55c23699805a7d8c61b1fba9
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-04-28 14:20:15 +0200

    initial import

---
+++ src/MyApp.ixx
@@ -0,0 +1,5 @@
+module;
+export module app;
+
+export namespace dev {
+};
---
+++ src/MyAppMain.cpp
@@ -0,0 +1,6 @@
+#include "Libraries.h"
+
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nCmdShow) {
+    Lysa::setHello();
+    return 0;
+}
