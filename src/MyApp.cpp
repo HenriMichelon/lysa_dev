25ea28c7579919b52c98dea96c8d7a5785371e33
Henri <148531229+HenriMichelon@users.noreply.github.com>
2025-05-21 14:15:16 +0200

    Index of index array

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -4,7 +4,8 @@ import main_scene;
 namespace app {
 
     lysa::ApplicationConfiguration appConfig {
-        .backend = vireo::Backend::VULKAN,
+        // .backend = vireo::Backend::VULKAN,
+        .backend = vireo::Backend::DIRECTX,
         .resourcesConfig = {
             .maxVertexInstances = 1000,
             .maxIndexInstances = 1500,
@@ -15,7 +16,6 @@ namespace app {
     lysa::WindowConfiguration windowConfig {
         .rootNode = std::make_shared<MainScene>(),
         .renderingConfig = {
-            // .backend = vireo::Backend::DIRECTX,
             .presentMode = vireo::PresentMode::IMMEDIATE,
             .clearColor = lysa::float3{0.0f, 0.2f, 0.4f},
         },
