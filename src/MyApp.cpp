5010e6037f8fb1f08aa6fddabad68c62d09a5712
Henri <148531229+HenriMichelon@users.noreply.github.com>
2025-05-25 21:03:15 +0200

    Multi windows

--- src/MyApp.cpp
+++ src/MyApp.cpp
@@ -4,57 +4,53 @@ import main_scene;
 namespace app {
 
     lysa::ApplicationConfiguration appConfig {
-        .backend = vireo::Backend::VULKAN,
-        // .backend = vireo::Backend::DIRECTX,
+        // .backend = vireo::Backend::VULKAN,
+        .backend = vireo::Backend::DIRECTX,
     };
 
     lysa::WindowConfiguration windowConfig {
-        .viewportConfig = {
-        },
+        .width = 800,
+        .height = 600,
+        .mainViewportConfig = {},
         .renderingConfig = {
             .presentMode = vireo::PresentMode::IMMEDIATE,
             .clearColor = lysa::float3{0.0f, 0.2f, 0.4f},
         },
     };
 
-    lysa::ViewportConfiguration viewport2Config{
-        .viewport = {
-            .y = -50,
-            .width = 400,
-            .height = 200,
-        }
-    };
-
     class MyWindow : public lysa::Window {
     public:
-        MyWindow(void* windowHandle) :
-        Window{windowConfig, windowHandle, std::make_shared<MainScene>()} {
-            viewport2Config.viewport.x = (getExtent().width - viewport2Config.viewport.width)/2;
-            addViewport(std::make_shared<lysa::Viewport>(viewport2Config))
-               ->setRootNode(std::make_shared<MainScene>());
+        MyWindow() :
+        Window{windowConfig, std::make_shared<MainScene>()} {
         }
     };
 
     class MyApp : public lysa::Application {
     public:
-        MyApp(void* windowHandle) :
-            Application{appConfig},
-            window{std::make_shared<MyWindow>(windowHandle)} {
-            addWindow(window);
+        MyApp() :
+            Application{appConfig} {
+            if (!lysa::dirExists("shaders")) {
+                throw lysa::Exception{"Shaders directory not found, please run the application from the root of the project"};
+            }
+
+            std::wstring title{L"LysaGame"};
+            if (appConfig.backend == vireo::Backend::VULKAN) {
+                title.append(L" (Vulkan)");
+            } else {
+                title.append(L" (DirectX)");
+            }
+            windowConfig.title = title;
+            windowConfig.x = 100;
+            addWindow(std::make_shared<MyWindow>());
+
+            windowConfig.x = 950;
+            addWindow(std::make_shared<MyWindow>());
         }
-
-        auto& getWindow() { return window; }
-
-    private:
-        std::shared_ptr<lysa::Window> window;
     };
-
-    constexpr auto WIDTH{1280};
-    constexpr auto HEIGHT{720};
 
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-#ifdef _WIN32
-#include "os/WindowsMain.cpp"
-#endif
+int lysaMain() {
+    app::MyApp{}.run();
+    return 0;
+}
