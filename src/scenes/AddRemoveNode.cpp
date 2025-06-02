ad2aacebf5e39d879c044e8333f1e42b48ca0a6f
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-06-02 15:56:35 +0200

    Non-texture assets support

--- src/scenes/AddRemoveNode.cpp
+++ src/scenes/AddRemoveNode.cpp
@@ -6,18 +6,17 @@ namespace app {
 
     void AddRemoveNodeScene::onReady() {
         BaseScene::onReady();
-        camera->setPosition(0.0f, 2.0f, 0.0f);
-        camera->rotateY(lysa::radians(45.0f));
-        // camera->setPosition(0.0f, 0.0f, 4.0f);
+        // camera->setPosition(0.0f, 2.0f, 0.0f);
+        // camera->rotateY(lysa::radians(45.0f));
+        camera->setPosition(0.0f, 0.0f, 4.0f);
         // camera->rotateX(lysa::radians(-45.0f));
         // camera->rotateY(lysa::radians(35.0f));
 
         addChild(std::make_shared<lysa::Environment>(lysa::float4{1.0, 1.0, 1.0, 1.0f}));
 
         cube = std::make_shared<Node>(L"Cube");
-        lysa::AssetsPack::load(*cube, L"app://res/models/sponza.assets");
-        // cube->setPosition(0.0f, 2.0f, 8.0f);
-        // cube->scale(.005f);
+        lysa::AssetsPack::load(*cube, L"app://res/models/survival_guitar_backpack.assets");
+        cube->scale(.01f);
         // cube->printTree();
         addChild(cube);
 
