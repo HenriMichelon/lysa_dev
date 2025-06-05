8935f1c5aa331d4fed9727ccb4571b018e793b35
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-06-05 14:55:37 +0200

    Indirect draw command generated by GPU

--- src/scenes/ViewAssets.cpp
+++ src/scenes/ViewAssets.cpp
@@ -6,15 +6,11 @@ namespace app {
 
     void ViewAssetsScene::onReady() {
         BaseScene::onReady();
-        // player->setRotation({0.000000,0.290285,0.000000,0.956941});
-        // player->setPosition({2.359956,-2.000000,2.579495});
-        // player->setRotation({0.000000,0.949024,0.000000,0.315239});
-        // player->setPosition({-5.947837,2.416667,-1.021150});
-        // player->setRotation({0.000000,-0.388723,0.000000,0.921357});
-        // player->setPosition({-2.403165,-1.500000,2.001667});
+        player->setRotation({0.000000,0.477159,0.000000,0.878818});
+        player->setPosition({4.306047,2.833333,0.506063});
 
         // player->rotateY(180.0f);
-        player->setPosition({0.0f,0.0f, 4.0f});
+        // player->setPosition({0.0f,0.0f, 4.0f});
 
         addChild(std::make_shared<lysa::Environment>(lysa::float4{1.0, 1.0, 1.0, 1.05f}));
 
@@ -23,7 +19,7 @@ namespace app {
         // addChild(dirLight);
 
         auto node  = std::make_shared<Node>(L"Cube");
-        lysa::AssetsPack::load(*node, L"app://res/models/sponza.assets");
+        lysa::AssetsPack::load(*node, L"app://res/models/crate.assets");
         // node->scale(.01f);
         addChild(node);
         // printTree();
